f58ec58cd995f246420e9eddd9c86e6891ed816a
JOAO PEDRO FREITAS DA SILVA <jpfdsilva@minha.fag.edu.br>
2025-03-15 21:16:28 -0300

    alterações para o pulll request

--- Aula2.java
+++ Aula2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Aula2 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);  
+        Scanner scanner = new Scanner(System.in);  //bem vindo
         int opcao;
 
         do {
